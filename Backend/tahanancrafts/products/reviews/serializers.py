651d84893463b768480daa1063b72c28e1b2e339
mykaachan <mykaelaperegrino@gmail.com>
2025-11-12 19:17:18 +0800

    ratings

--- Backend/tahanancrafts/products/reviews/serializers.py
+++ Backend/tahanancrafts/products/reviews/serializers.py
@@ -5,7 +5,7 @@ from users.models import CustomUser
 
 
 class ProductRatingSerializer(serializers.ModelSerializer):
-    user_name = serializers.CharField(source='user.username', read_only=True)
+    user_name = serializers.CharField(source='user.name', read_only=True)
     product_name = serializers.CharField(source='product.name', read_only=True)
 
     class Meta:
