6854b54d251fefd0cb9cdf13726b97b4a8fda636
mykaachan <mykaelaperegrino@gmail.com>
2025-12-04 12:49:48 +0800

    Okay na ang My-purchase

--- Frontend/src/ChatPopup.js
+++ Frontend/src/ChatPopup.js
@@ -2,8 +2,8 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
-//const API = "https://tahanancrafts.onrender.com"; 
-const API = "http://127.0.0.1:8000";
+const API = "https://tahanancrafts.onrender.com"; 
+//const API = "http://127.0.0.1:8000";
 
 function ChatPopup() {
   const [open, setOpen] = useState(false);
